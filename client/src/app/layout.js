76b4ef3e39465f6e601fd0027879563c8e357f1c
SushilKushwaha <sushilsinha733@gmail.com>
2024-01-24 08:43:39 +0545

    redux directory changed

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import {Providers} from "./providers";
-import ReduxProvider from "./redux/reduxProvider"
+import ReduxProvider from "../redux/reduxProvider"
 
 const inter = Inter({ subsets: ['latin'] })
 
