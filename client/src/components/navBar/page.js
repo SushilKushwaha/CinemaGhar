2eccc2dc2c1f011c53d1f9e76fc70dfaa4418108
SushilKushwaha <sushilsinha733@gmail.com>
2024-04-11 12:07:40 +0545

    In search bar list the movies from backend

--- client/src/components/navBar/page.js
+++ client/src/components/navBar/page.js
@@ -1,13 +1,22 @@
 'use client'
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Input, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, Avatar} from "@nextui-org/react";
 import Image from "next/image";
 import {FaSearch} from 'react-icons/fa';
 import Loginmodal from '@/app/login/loginmodal'
+import { getAllMovies } from "@/api-helpers/api-helpers";
 export default function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getAllMovies().then((data) => setMovies(data.movies)).catch((error) => console.log(error));
+  }, []);
+
   return (
-    <Navbar isBordered>
+    
+    <Navbar isBordered className="bg-zinc-300">
       <NavbarContent justify="start">
         <NavbarBrand className="mr-4">
           <Image src="/cinemaghar.jpg" width = "150" height="150" alt="" />
@@ -19,12 +28,22 @@ export default function App() {
 
       <NavbarContent as="div" className="items-center" justify="end">
       <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder="Search movies..." className='bg-transparent focus:outline-none w-24 sm:w-64' />
-            <button>
-              <FaSearch className='text-slate-600' />
-            </button>
-           
-        </form>
+    <input 
+        type="text" 
+        placeholder="Search movies..." 
+        className='bg-transparent focus:outline-none w-24 sm:w-64' 
+        list="movieOptions" 
+    />
+    <datalist id="movieOptions"> 
+        {movies && movies.map((option) => ( 
+            <option key={option._id} value={option.title} /> 
+        ))}
+    </datalist>
+    <button>
+        <FaSearch className='text-slate-600' />
+    </button>
+</form>
+
       <NavbarItem>
       <Dropdown>
       <DropdownTrigger>
