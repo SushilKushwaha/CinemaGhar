708e8ae9c9e088b59504e6c44fe26a81d7fd66bd
SushilKushwaha <sushilsinha733@gmail.com>
2024-05-08 11:37:24 +0545

    get all the movies details form backend to booking page

--- frontend/src/components/Bookings/Booking.jsx
+++ frontend/src/components/Bookings/Booking.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { getAllMoviesDetails } from '../../api-helpers/api-helpers';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 const Booking = () => {
 
@@ -21,6 +21,24 @@ const Booking = () => {
           <Typography padding={3} fontFamily='fantasy' variant='h4' textAlign={"center"}>
             Book Tickets of Movie: {movie.title}
           </Typography>
+          <Box display={"flex"} justifyContent={"center"}>
+            <Box display={"flex"} justifyContent={"column"} flexDirection="column" padding={3} width="50%" marginRight={"auto"}>
+              <img width="30%" height={"300px"} src={movie.posterUrl} alt={movie.title} />
+              <Box width={"80%"} marginTop={3} padding={2}>
+                <Typography paddingTop={2}>
+                  {movie.description}
+                </Typography>
+                <Typography fontWeight={"bold"} marginTop={1}>
+                  Actors: {movie.actors.map((actor) => " " + actor + ",")}
+                </Typography>
+                <Typography fontWeight={'bold'} marginTop={1}>
+                  Release Date: {new Date(movie.releaseDate).toDateString()}
+                </Typography>
+              </Box>
+
+            </Box>
+
+          </Box>
         </Fragment>
       )};
     </div>
